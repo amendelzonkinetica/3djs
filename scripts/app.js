16b0cce6eae5ff68aad7073cc72ff8df1bf62357
Ariel Mendelzon <ariel.mendelzon@kinetica-solutions.com>
2014-08-28 11:01:19 -0300

    Esqueleto vacio con require

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+define([], function() {
+	var app = function() {
+	};
+	
+	app.prototype.run = function() {
+		alert('Hello, world!');
+	};
+	
+	return app;
+});
---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+﻿// Require configuration
+require.config({
+	waitSeconds: 15,
+	urlArgs: '_=' + (new Date()).getTime().toString()
+});
+
+// Run
+require(['app'], function (App) {
+	var app = new App();
+	app.run();
+});
